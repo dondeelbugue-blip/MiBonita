90e2cb89e5b4443a884a76a7e95062c4101904ac
dondeelbugue-blip <dondeelbugue@gmail.com>
2026-02-14 07:14:10 -0500

    Tarjeta de San Valentín para mí Bonita - Proyecto completo

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    lines: [
+        "Para mí Bonita",
+        "Aunque todavía tenemos que aprender mucho sobre el amor, quiero hacerlo contigo.",
+        "Aunque tengo mucho por mejorar, quiero mejorarlas por nosotros.",
+        "Aunque aun no alcanzo mi mejor versión, solo quiero que seas tu quien la disfrute...",
+        "Aunque tengo mucho por vivir, quiero vivirlo a tu lado.",
+    ],
+    date: "2017-07-20T20:15:00",
+    names: ["Para mí Bonita", ""]
+}
